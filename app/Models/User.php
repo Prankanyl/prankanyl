b0c94b7dda379ff495a6976e1d1f53e80af0fc00
Prankanyl <v752433@icloud.com>
2021-07-25 11:29:11 +0300

    install backpack

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Models;
-
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, CrudTrait, HasRoles;
 
     /**
      * The attributes that are mass assignable.
