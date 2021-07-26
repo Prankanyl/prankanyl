44e870f033148f83f666d2d9695599539e90eda0
Prankanyl <v752433@icloud.com>
2021-07-26 09:56:48 +0300

    added footer, header nav component and added some pages

---
+++ resources/lang/en/static.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    // Default title
+    'home' => 'Home',
+    'login' => 'Login',
+    'image' => 'Image',
+    'title' => 'Title',
+    'logo' => 'Logo',
+    'description' => 'Description',
+];
