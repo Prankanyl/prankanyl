93bb6c6387c411c3afcace5547e288b90913e534
Prankanyl <v752433@icloud.com>
2021-07-30 16:09:37 +0300

    refactor backpack sidebar, added lang file for sidebar

---
+++ resources/lang/en/sidebar.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'crm' => 'CRM',
+    'cms' => 'CMS',
+    'articles' => 'Articles',
+    'article_categories' => 'Article Categories',
+    'projects' => 'Projects',
+    'project_categories' => 'Project Categories',
+    'project_types' => 'Project Types',
+    'development_tools' => 'Development Tools',
+    'site_information' => 'Site Information',
+    'social_networks' => 'Social Networks',
+
+    'settings' => 'Settings',
+    'general_settings' => 'General Settings',
+    'project_settings' => 'Project Settings',
+    'article_settings' => 'Article Settings',
+    'site_settings' => 'Site Settings',
+];
